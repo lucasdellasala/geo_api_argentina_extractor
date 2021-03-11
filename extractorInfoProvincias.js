e9ac1925a1eebd5fa11dbcbdc8a8bb0501b0f26d
Lucas Della Sala <lucasdellasalamarkovic@gmail.com>
2021-03-11 16:41:06 -0300

    Resolviendo doble promise

--- extractorInfoProvincias.js
+++ extractorInfoProvincias.js
@@ -26,6 +26,41 @@ const getProvinceDepartments = (idProvince) => {
     })
 }
 
+const getDepartmentsWithCorrectCentroids = (provinceData, idProvince) => {
+    const urlLocalidadesCensales = `https://apis.datos.gob.ar/georef/api/localidades-censales?provincia=${idProvince}&max=600`
+
+    return new Promise((resolve, reject) => {
+        //CABA: "02"
+        if (idProvince != "02") {
+            fetch(urlLocalidadesCensales)
+                .then(response => response.json())
+                .then(data => {
+                    const correctedData = centroidCorrector(provinceData, data)
+                    resolve(correctedData)
+                })
+                .catch(err => reject(err))
+        } else {
+            //Esto lo hago para en lugar de poner las Comunas de CABA poner los barrios.
+            resolve(provinceData)
+        }
+    })
+}
+
+const centroidCorrector = (provinceData, newData) => {
+    const cabecera = 'CABECERA_DEPARTAMENTO'
+    const partidos = provinceData.departamentos
+    const localidades = newData.localidades_censales
+    const partidosCorregidos = provinceData
+    console.log(localidades[0].provincia.nombre)
+    for (let i = 0; i < partidos.length; i++) {
+        const cabeceraDepartamento = localidades.find(localidad => localidad.departamento.id === partidos[i].id && localidad.funcion === cabecera)
+
+        partidosCorregidos.departamentos[i].centroide = cabeceraDepartamento.centroide
+    }
+
+    return partidosCorregidos
+}
+
 const camelize = (text) => {
     return text.replace(/^([A-Z])|[\s-_]+(\w)/g, function (match, p1, p2, offset) {
         if (p2) return p2.toUpperCase();
@@ -40,15 +75,22 @@ const removeAccents = (str) => {
 for (let i = 0; i < listadoProvincias.length; i++) {
     const idProvince = listadoProvincias[i].id
     const provinceName = removeAccents(camelize(listadoProvincias[i].nombre))
-    getProvinceDepartments(idProvince).then(
-        (provinceData) => {
-            fs.writeFile(`./extraerAqui${provinceName}.json`, JSON.stringify(provinceData), (err) => {
-                if (err) {
-                    console.error(err)
-                } else {
-                    console.log(`File '${provinceName}.json' successfully written!`)
-                }
-            })
-        }
-    ).catch(err => console.error(err))
+    getProvinceDepartments(idProvince)
+        .then(
+            (provinceData) => {
+                
+                getDepartmentsWithCorrectCentroids(provinceData, idProvince)
+                    .then(correctedProvinceData => {
+                        fs.writeFile(`./extraerAqui/${idProvince}.json`, JSON.stringify(correctedProvinceData), (err) => {
+                            if (err) {
+                                console.error(err)
+                            } else {
+                                console.log(`File '${idProvince}.json' successfully written!`)
+                            }
+                        })
+                    })
+                    .catch(err => console.error(err))
+            }
+        )
+        .catch(err => console.error(err))
 }
